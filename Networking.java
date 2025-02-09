12dfe7950c940bc592f815538e0957384b6fc8a2
Sameer <bhandarisameer173@gmail.com>
2025-02-09 19:18:11 +0545

    pratical 1

--- Networking.java
+++ Networking.java
@@ -2,8 +2,8 @@ import java.net.*;
 
 public class Networking {
     public static void main(String[] args) throws UnknownHostException {
-        InetAddress obj1 = InetAddress.getByName("www.example.com");
-        InetAddress obj2= InetAddress.getByName("example.com");
+        InetAddress obj1 = InetAddress.getByName("mywebsite.com");
+        InetAddress obj2= InetAddress.getByName("www.mywebsite.com");
         boolean x = obj1.equals(obj2);
         System.out.println("Both hostnames are equal:"+x);
            
