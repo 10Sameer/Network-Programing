8b8d8e903b2aa48f9166c4d99ca8fbff0f9b1c8c
Sameer <bhandarisameer173@gmail.com>
2025-02-11 20:52:43 +0545

    pratical_4

--- hostIPFinderPractical3.java
+++ hostIPFinderPractical3.java
@@ -15,7 +15,7 @@ public class hostIPFinderPractical3 {
 
     public static void main(String[] args) {
         // Given hostnames
-        String[] hostNames = {"www.scst.edu.np", "www.google.com", "www.facebook.com"};
+        String[] hostNames = {"sagarmatha.edu.np", "www.google.com", "www.facebook.com"};
 
         // Get and print IP address for each host
         for (String host : hostNames) {
