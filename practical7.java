9b6ca5a1a93edd59d165e46e574ace6ae6c219b7
Sameer <bhandarisameer173@gmail.com>
2025-03-19 20:01:44 +0545

    Np lab 7

---
+++ practical7.java
@@ -0,0 +1,22 @@
+import java.net.URL;
+
+
+public class practical7 {
+    public static void compareURLs(String url1, String url2) {
+        try {
+            URL u1 = new URL(url1);
+            URL u2 = new URL(url2);
+            
+            System.out.println("Comparing: " + url1 + " and " + url2);
+            System.out.println("Hosts are same: " + u1.getHost().equals(u2.getHost()));
+            System.out.println();
+        } catch (Exception e) {
+            System.out.println("Invalid URL(s)");
+        }
+    }
+
+    public static void main(String[] args) {
+        compareURLs("http://www.ibiblio.org", "http://helios.ibiblio.org");
+        compareURLs("http://www.ibiblio.org/", "http://ibiblio.org/");
+    }
+}
