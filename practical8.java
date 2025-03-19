f5f27f235b5b34a19e2b318312a61f56b7be3306
Sameer <bhandarisameer173@gmail.com>
2025-03-19 20:28:32 +0545

    Np lab 8

---
+++ practical8.java
@@ -0,0 +1,33 @@
+import java.net.*;
+import java.io.*;
+
+
+public class practical8 {
+    public static void main(String[] args) throws Exception {
+        
+        CookiePolicy customPolicy = new CookiePolicy() {
+            @Override
+            public boolean shouldAccept(URI uri, HttpCookie cookie) {
+                String host = uri.getHost();
+                if (host != null && host.endsWith(".gov")) {
+                    return false; 
+                }
+                return true;  
+            }
+        };
+
+        CookieManager manager = new CookieManager(null, customPolicy);
+        CookieHandler.setDefault(manager);
+
+       
+        URL url = new URL("https://jsonplaceholder.typicode.com/posts/1");
+        URLConnection connection = url.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine); 
+        }
+        in.close();
+    }
+}
