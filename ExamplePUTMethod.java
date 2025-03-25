40cf9290367a4ed2d301fdd8acdc84826f150f64
Sameer <bhandarisameer173@gmail.com>
2025-03-25 21:25:37 +0545

    Put Method

---
+++ ExamplePUTMethod.java
@@ -0,0 +1,20 @@
+import java.net.*;
+import java.io.*;
+
+public class ExamplePUTMethod  {
+    public static void main(String[] args) throws Exception {
+        URL url = new URL("http://example.com/api/update");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("PUT");
+        conn.setDoOutput(true);
+
+        // Send data in the request body
+        String data = "name=JohnDoe&email=john@example.com";
+        try (OutputStream os = conn.getOutputStream()) {
+            byte[] input = data.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+
+        System.out.println("Response Code: " + conn.getResponseCode());
+    }
+}
