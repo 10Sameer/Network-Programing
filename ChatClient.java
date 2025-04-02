dd474ef281660a3f5b5519a2800c67cfe48b678d
Sameer <bhandarisameer173@gmail.com>
2025-04-02 21:11:50 +0545

    Recieve data

--- ChatClient.java
+++ ChatClient.java
@@ -4,7 +4,7 @@ import java.net.*;
 public class ChatClient {
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("localhost", 6100); 
+            Socket socket = new Socket("localhost", 6200); 
             System.out.println("Connected to server.");
 
             // Input and Output streams
--- ChatServer.java
+++ ChatServer.java
@@ -4,7 +4,7 @@ import java.net.*;
 public class ChatServer {
     public static void main(String[] args) {
         try {
-            ServerSocket serverSocket = new ServerSocket(6100); // Create server socket on port 5000
+            ServerSocket serverSocket = new ServerSocket(6200); // Create server socket on port 5000
             System.out.println("Server started . Waiting for client...");
             Socket socket = serverSocket.accept(); // Accept client connection
             System.out.println("Client connected.");
