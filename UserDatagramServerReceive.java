8d3c92e4b0131f59110a821cff015e8838850f1a
Sameer <bhandarisameer173@gmail.com>
2025-04-07 21:26:50 +0545

    Datagram Recieve

--- UserDatagramServerReceive.java
+++ UserDatagramServerReceive.java
@@ -16,4 +16,18 @@ public class UserDatagramServerReceive {
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, serverAddress, 9876);
             clientSocket.send(sendPacket);
 
-      
+            // Receive reply from server
+            byte[] receiveData = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+
+            String serverReply = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.println("Reply from server: " + serverReply);
+
+            clientSocket.close();
+            sc.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
