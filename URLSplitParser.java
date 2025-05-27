9eb7a00b80ed4402bbc12f951bba3ff0c0e6f064
Sameer <bhandarisameer173@gmail.com>
2025-05-27 21:13:45 +0545

    URL split

--- URLSplitParser.java
+++ URLSplitParser.java
@@ -14,6 +14,17 @@ public class URLSplitParser {
         String path = "/" + (hostSplit.length > 1 ? hostSplit[1] : "");
 
         // Split host and port if port is specified
-        
+        String host;
+        String port = "default";
+        if (hostPort.contains(":")) {
+            String[] hostPortSplit = hostPort.split(":");
+            host = hostPortSplit[0];
+            port = hostPortSplit[1];
+        } else {
+            host = hostPort;
+            port = protocol.equals("https") ? "443" : "80"; // Default ports
+        }
+
+   
     }
 }
