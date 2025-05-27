36935f41f2296ae6335be1c9f8457ccad378b24f
Sameer <bhandarisameer173@gmail.com>
2025-05-27 21:09:56 +0545

    Ip

--- NetworkInfo.java
+++ NetworkInfo.java
@@ -11,6 +11,21 @@ public class NetworkInfo {
             // Get the network interface associated with the IP
             NetworkInterface network = NetworkInterface.getByInetAddress(ip);
 
+            if (network != null) {
+                byte[] mac = network.getHardwareAddress();
+
+                if (mac != null) {
+                    StringBuilder macAddress = new StringBuilder();
+                    for (int i = 0; i < mac.length; i++) {
+                        macAddress.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));
+                    }
+                    System.out.println("MAC Address: " + macAddress.toString());
+                } else {
+                    System.out.println("MAC Address not available.");
+                }
+            } else {
+                System.out.println("Network Interface for the IP not found.");
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
