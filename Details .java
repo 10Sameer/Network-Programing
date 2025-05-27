d465f17aadd08370ffa7e8d07a63219a03c5391f
Sameer <bhandarisameer173@gmail.com>
2025-05-27 21:12:03 +0545

    Ip details

--- Details .java
+++ Details .java
@@ -11,7 +11,15 @@ public class Details {
             String host = url.getHost();
 
             // Resolve host to IP
-    
+            InetAddress address = InetAddress.getByName(host);
+
+            System.out.println("URL: " + urlString);
+            System.out.println("Host: " + host);
+            System.out.println("IP Address: " + address.getHostAddress());
+
+            // Optional: Check reachability
+            boolean reachable = address.isReachable(5000); // 5 second timeout
+            System.out.println("Is Host Reachable? " + (reachable ? "Yes" : "No"));
 
         } catch (Exception e) {
             e.printStackTrace();
