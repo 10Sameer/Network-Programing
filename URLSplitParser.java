ddb0f7d69b4e123360644673a9b48bae7df7aa30
Sameer <bhandarisameer173@gmail.com>
2025-05-27 21:14:05 +0545

    URL split

--- URLSplitParser.java
+++ URLSplitParser.java
@@ -35,6 +35,22 @@ public class URLSplitParser {
         String query = "";
         String fragment = "";
 
-  
+        if (path.contains("?")) {
+            String[] pathQuerySplit = path.split("\\?", 2);
+            path = pathQuerySplit[0];
+            String[] queryFragmentSplit = pathQuerySplit[1].split("#", 2);
+            query = queryFragmentSplit[0];
+            if (queryFragmentSplit.length > 1) {
+                fragment = queryFragmentSplit[1];
+            }
+        } else if (path.contains("#")) {
+            String[] pathFragmentSplit = path.split("#", 2);
+            path = pathFragmentSplit[0];
+            fragment = pathFragmentSplit[1];
+        }
+
+        System.out.println("Parsed Path: " + path);
+        System.out.println("Query: " + query);
+        System.out.println("Fragment: " + fragment);
     }
 }
