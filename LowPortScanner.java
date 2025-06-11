f50da72ac6c15af111a72ae4a722c773f4f5ac1e
Sameer <bhandarisameer173@gmail.com>
2025-06-11 21:54:57 +0545

    Low Port Scanner

---
+++ LowPortScanner.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+
+public class LowPortScanner {
+
+    public static void main(String[] args) {
+        System.out.println("Scanning TCP and UDP ports (1-1024) on localhost...\n");
+
+        for (int port = 1; port <= 1024; port++) {
+            if (isTCPPortOpen(port)) {
+                System.out.println("TCP Port " + port + " is OPEN");
+            }
+       
