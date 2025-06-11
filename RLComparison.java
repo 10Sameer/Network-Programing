dc79e54bd162597ae9d263b05c860a9ce93816cd
Sameer <bhandarisameer173@gmail.com>
2025-06-11 21:56:55 +0545

    URL comparison

--- RLComparison.java
+++ RLComparison.java
@@ -5,4 +5,13 @@ public class URLComparison {
         try {
             URL u1 = new URL(url1);
             URL u2 = new URL(url2);
-     
+            
+            System.out.println("Comparing: " + url1 + " and " + url2);
+            System.out.println("Hosts are same: " + u1.getHost().equals(u2.getHost()));
+            System.out.println();
+        } catch (Exception e) {
+            System.out.println("Invalid URL(s)");
+        }
+    }
+
+
