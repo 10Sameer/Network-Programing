2808cf2c587dbcde9a09a72d470510c83a51e3a5
Sameer <bhandarisameer173@gmail.com>
2025-06-11 21:56:46 +0545

    URL comparison

---
+++ RLComparison.java
@@ -0,0 +1,8 @@
+import java.net.URL;
+
+public class URLComparison {
+    public static void compareURLs(String url1, String url2) {
+        try {
+            URL u1 = new URL(url1);
+            URL u2 = new URL(url2);
+     
