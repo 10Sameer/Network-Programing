56ae5675abe65ac422545536cd3caca83607abf7
Sameer <bhandarisameer173@gmail.com>
2025-06-19 21:02:53 +0545

    Lab-8

--- DaytimeSameer.java
+++ DaytimeSameer.java
@@ -1,1 +1,7 @@
+import java.io.*;
+import java.net.*;
 
+public class DaytimeSameer {
+    public static void main(String[] args) {
+        String serverIP = "192.168.1.10"; 
+        int port = 5055;
