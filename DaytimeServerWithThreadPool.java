8a37c555cf14042179c8ef6ac050d95cb3ca0438
Sameer <bhandarisameer173@gmail.com>
2025-06-21 18:25:50 +0545

    Lab 13

--- DaytimeServerWithThreadPool.java
+++ DaytimeServerWithThreadPool.java
@@ -9,5 +9,25 @@ public class DaytimeServerWithThreadPool {
     private static final int PORT = 5000;
     private static final int THREAD_POOL_SIZE = 10;
 
+    public static void main(String[] args) {
+        ExecutorService threadPool = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Daytime server with Thread Pool Started on port " + PORT);
+
+            while (true) {
+                try {
+                    Socket clientSocket = serverSocket.accept();
+                    System.out.println("Client connected: " + clientSocket.getInetAddress().getHostAddress());
+                    threadPool.submit(() -> handleClient(clientSocket));
+                } catch (IOException e) {
+                    System.err.println("Error accepting client: " + e.getMessage());
+                }
+            }
+
+        } catch (IOException e) {
+            System.err.println("Server error: " + e.getMessage());
+        }
+    }
 
    
