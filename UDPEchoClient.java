8eedff1b3eab75ca9888536d102600f7e928d233
Sameer <bhandarisameer173@gmail.com>
2025-08-05 18:13:37 +0545

    abc

--- UDPEchoClient.java
+++ UDPEchoClient.java
@@ -11,4 +11,24 @@ public class UDPEchoClient {
         try (DatagramSocket clientSocket = new DatagramSocket();
              Scanner scanner = new Scanner(System.in)) {
 
-        
+            System.out.print("Enter message to send: ");
+            String message = scanner.nextLine();
+
+            byte[] sendBuffer = message.getBytes();
+            InetAddress serverAddress = InetAddress.getByName(serverIP);
+
+            DatagramPacket sendPacket = new DatagramPacket(sendBuffer, sendBuffer.length, serverAddress, port);
+            clientSocket.send(sendPacket);
+
+            byte[] receiveBuffer = new byte[1024];
+            DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+            clientSocket.receive(receivePacket);
+
+            String echoedMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            System.out.println("Received from server: " + echoedMessage);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
