afbf49a183dcd69a71e3a01870a2d9400f2776d6
Sameer <bhandarisameer173@gmail.com>
2025-08-05 18:12:25 +0545

    abc

--- UDPEchoServer.java
+++ UDPEchoServer.java
@@ -13,3 +13,37 @@ public class UDPEchoServer {
             while (true) {
                 DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
                 serverSocket.receive(receivePacket);
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class UDPEchoServer {
+    public static void main(String[] args) {
+        int port = 9876;
+
+        try (DatagramSocket serverSocket = new DatagramSocket(port)) {
+            System.out.println("UDP Echo Server is running on port " + port + "...");
+
+            byte[] receiveBuffer = new byte[1024];
+
+            while (true) {
+                DatagramPacket receivePacket = new DatagramPacket(receiveBuffer, receiveBuffer.length);
+                serverSocket.receive(receivePacket);
+
+                String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+                System.out.println("Received from client: " + clientMessage);
+
+                // Echo the same message back
+                DatagramPacket sendPacket = new DatagramPacket(
+                        receivePacket.getData(),
+                        receivePacket.getLength(),
+                        receivePacket.getAddress(),
+                        receivePacket.getPort()
+                );
+
+                serverSocket.send(sendPacket);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
